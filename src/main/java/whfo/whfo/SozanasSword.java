1b950654d203f5763a21c34857de2e904fe17464
BuildTools <unconfigured@null.spigotmc.org>
2021-03-22 12:38:19 -0600

    added emerald tool textures and update sozanas sword

--- src/main/java/whfo/whfo/SozanasSword.java
+++ src/main/java/whfo/whfo/SozanasSword.java
@@ -12,6 +12,7 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.vector.Vector3f;
 import net.minecraft.world.World;
 
 import java.util.Vector;
@@ -57,12 +58,12 @@ public class SozanasSword extends SwordItem {
         return true;
     }
 
-    //code is incomplete UNCOMMENTING WILL BREAK THE MOD
-    /*
+    //code is incomplete and may not work for stability comment it out
+
     @Override
     public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand) {
-        world.addEntity(new FireballEntity(EntityType.FIREBALL, world));
+        FireballEntity fb = new FireballEntity(EntityType.FIREBALL, world);
+        fb.setMotion(player.getLookVec());
         return super.onItemRightClick(world, player, hand);
     }
-     */
 }
